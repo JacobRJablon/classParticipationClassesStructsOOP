e2f2cd6dcd61c1b316bd222e4fb50d9c3c762b38
JacobRJablon <jacob.r.jablon@gmail.com>
2024-10-24 08:25:20 -0400

    creating basic class structure

--- classesStructsOOP/classesStructsOOP/Program.cs
+++ classesStructsOOP/classesStructsOOP/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace classesStructsOOP
 {
+    public class Character
+    {
+        public string name;
+        public int exp = 0;
+
+        public Character()
+        {
+            name = "Not assigned";
+        }
+
+        public Character(string name)
+        {
+            this.name = name;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Character hero = new Character();
+            Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
+
+            Character heroine = new Character("Agatha");
+            Console.WriteLine("Hero: " + heroine.name + " - " + heroine.exp + " EXP");
         }
     }
 }
