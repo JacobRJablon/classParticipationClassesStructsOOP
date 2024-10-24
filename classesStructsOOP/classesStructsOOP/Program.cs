ddacdb78cc786f6be862339ccb8cd6654668e5de
JacobRJablon <jacob.r.jablon@gmail.com>
2024-10-24 08:17:55 -0400

    initial commit

---
+++ classesStructsOOP/classesStructsOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace classesStructsOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
