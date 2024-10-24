3a2841228ae46c3417ebc2c7e74d6ed8546907d0
JacobRJablon <jacob.r.jablon@gmail.com>
2024-10-24 08:48:52 -0400

    Finished the base of the program with classes in the main file

--- classesStructsOOP/classesStructsOOP/Program.cs
+++ classesStructsOOP/classesStructsOOP/Program.cs
@@ -14,7 +14,49 @@
         {
             this.name = name;
         }
+
+        public virtual void PrintStatsInfo()
+        {
+            Console.WriteLine("Hero: " + this.name + " - " + this.exp + " EXP");
+        }
+
+        private void Reset()
+        {
+            this.name = "Not assigned";
+            this.exp = 0;
+        }
     }
+
+    public class Paladin: Character
+    {
+        public Weapon weapon;
+
+        public Paladin(string name, Weapon weapon): base(name)
+        {
+            this.weapon = weapon;
+        }
+        public override void PrintStatsInfo()
+        {
+            Console.WriteLine("Hail " + this.name + " - take up your " + this.weapon.name + "!");
+        }
+    }
+    public struct Weapon
+    {
+        public string name;
+        public int damage;
+
+        public Weapon(string name, int damage)
+        {
+            this.name = name;
+            this.damage = damage;
+        }
+
+        public void PrintWeaponStats()
+        {
+            Console.WriteLine("Weapon: " + this.name + " - " + this.damage + " DMG");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -22,10 +64,45 @@
             Console.WriteLine("Hello, World!");
 
             Character hero = new Character();
-            Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
+            hero.PrintStatsInfo();
 
             Character heroine = new Character("Agatha");
-            Console.WriteLine("Hero: " + heroine.name + " - " + heroine.exp + " EXP");
+            heroine.PrintStatsInfo();
+
+            Weapon huntingBow = new Weapon("Hunting Bow", 105);
+            huntingBow.PrintWeaponStats();
+
+            //Testing reference types
+            Character villian = hero;
+
+            hero.PrintStatsInfo();
+            villian.PrintStatsInfo();
+
+            villian.name = "The Joker";
+
+            hero.PrintStatsInfo();
+            villian.PrintStatsInfo();
+
+            //Testing value types
+            //note: hunting bow is declared above
+            Weapon warBow = huntingBow;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
+
+            warBow.name = "War Bow";
+            warBow.damage = 155;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
+
+            //hero.Reset();
+
+            //Experimenting with Inheritance
+            Paladin knight = new Paladin("Sir Arthur", huntingBow);
+            knight.PrintStatsInfo();
+
+
         }
     }
 }
