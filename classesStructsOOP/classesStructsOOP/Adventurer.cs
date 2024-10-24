b6282c4aa9f02fa7a0de500becf708725f2a3315
JacobRJablon <jacob.r.jablon@gmail.com>
2024-10-24 09:03:38 -0400

    Added classes in external files

---
+++ classesStructsOOP/classesStructsOOP/Adventurer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Adventurer
+{
+	public string name;
+	public int experience = 0;
+
+	public Adventurer(string name)
+	{
+		this.name = name;
+	}
+
+	public virtual void PrintStatsInfo()
+	{
+        Console.WriteLine("Adventurer: " + this.name + " - " + this.experience + " EXP");
+    }
+
+	private void Reset()
+	{
+		this.name = "Not Assigned";
+		this.experience = 0;
+	}
+}
